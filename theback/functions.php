57c925c1bb438f1459df75773282fbfa01993fd3
Srikanth Namburu <snamburu@adaequare.com>
2017-09-11 22:13:11 +0530

    Added mysql calls

--- theback/functions.php
+++ theback/functions.php
@@ -16,15 +16,19 @@ if ($_POST['action'] == 'PRICES') {
     $urgent = $_POST['urgent'];
     $immediate = $_POST['immediate'];
     $date = date('Y-m-d H:i:s', time());
-    $query1 = mysql_query('SELECT * FROM visa_prices WHERE id=1');
-    if (mysql_num_rows($query1) > 0) {
-        $query = "UPDATE visa_prices SET normal=" . $normal . ", urgent=" . $urgent . ", immediate=" . $immediate . ", date_modified" . $date . " WHERE id=1";
+    $query1 = $mysqli->query('SELECT * FROM visa_prices WHERE id=1');
+    if ($query1->num_rows > 0) {
+        $query = "UPDATE visa_prices SET normal=" . $normal . ", urgent=" . $urgent . ", immediate=" . $immediate . ", date_modified='" . $date . "' WHERE id=1";
         $_SESSION['errorMsg'] = '<p style="color:green">Successfully Updated Prices</p>';
     } else {
-        $query = "INSERT INTO visa_prices SET normal=" . $normal . ", urgent=" . $urgent . ", immediate=" . $immediate . ", date_modified" . $date;
+        $query = "INSERT INTO visa_prices SET normal=" . $normal . ", urgent=" . $urgent . ", immediate=" . $immediate . ", date_modified='" . $date."'";
         $_SESSION['errorMsg'] = '<p style="color:green">Successfully Inserted Prices</p>';
     }
-    mysql_query($query);
+
+    if (!$mysqli->query($query)) {
+        $err = $st->errorInfo();
+        die("Error in query: $query . " . $mysqli->error);
+    }
 
     header("Location: prices.php");
 }
